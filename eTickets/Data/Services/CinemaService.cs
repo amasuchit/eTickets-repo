c682079a860ed33554c11f79c6e4e77b309dd491
amasuchit <amatyasuchit@gmail.com>
2025-04-29 15:33:49 +0545

    producerCRUD

---
+++ eTickets/Data/Services/CinemaService.cs
@@ -0,0 +1,16 @@
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public class CinemaService : EntityBaseRepository<Cinema>, ICinemaService
+    {
+        private readonly AppDbContext context;
+        public CinemaService(AppDbContext context) : base(context)
+        {
+            this.context = context;
+        }
+    }
+    {
+    }
+}
